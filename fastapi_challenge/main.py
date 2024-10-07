239064fcf3f1123e8dbd710da9c6df4dc65e6e9f
Borna <Borna1103@msn.com>
2024-10-06 20:07:22 -0700

    changes

--- fastapi_challenge/main.py
+++ fastapi_challenge/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 # from pydantic import BaseModel
 
 app = FastAPI()
 
-#delete when ready to create the endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-# Define a Pydantic model for the request body with name, studying, and age
-
+# Define a Pydantic model for the request body with name, studying, and age/hobbies
+class AboutMe(BaseModel):
+    name: str
+    study: str
+    hobbies: str
+    
 # Define a POST endpoint that receives Intern details and returns them
+
+users = []
+
+@app.post("/users")
+async def post_user(text: AboutMe):
+    for user in users:
+        if text.name == user.name:
+            raise HTTPException(status_code=403, detail=f"{text.name} is already in the database")
+    
+    users.append(text);
+    return text
+
+@app.get("/users")
+async def get_users():
+    return users
